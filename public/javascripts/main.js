38ecd0d399a445250b908af864e10ec8d7d4beec
sshworld <ssh97540@gmail.com>
2020-11-19 17:02:15 +0900

    main 화면 구현

---
+++ public/javascripts/main.js
@@ -0,0 +1,11 @@
+
+window.addEventListener("scroll", function() {
+    let menuArea = document.getElementById('menu-area')
+
+    if(window.pageYOffset > 0) {
+        menuArea.classList.add('cus-nav')
+    } 
+    else {
+        menuArea.classList.remove('cus-nav')
+    }
+})
