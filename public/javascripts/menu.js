f44b7955410c6962b53a26af93c1fe23ea7c3bde
sshworld <ssh97540@gmail.com>
2020-11-22 22:13:08 +0900

    nav 고치자

--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -6,10 +6,12 @@ function visi() {
 
     if (menuicon) {
         document.getElementById('menu').style.display = 'flex'
+        document.getElementById('nav').style.display = 'none'
         document.getElementById('menu').style.transition = '1s'
         
     } else {
         document.getElementById('menu').style.display = 'none'
+        document.getElementById('nav').style.display = 'flex'
         
     }
 }
