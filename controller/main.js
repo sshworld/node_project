d3f789539e7f667390c7737695b2fcaefd4f148d
moon <yunii23@naver.com>
2020-11-23 20:21:23 +0900

    주석 처리

--- controller/main.js
+++ controller/main.js
@@ -1,6 +1,7 @@
 const db = require("../middleware/db")
 
 class mainController {
+    // 베스트 3개 가져오기
     async selectMain (req, res, next) {
 
         const searchInfo = await db(`SELECT * FROM recipe WHERE recipe_name LIKE "%${req.session.recipe_name}%"`)
@@ -18,6 +19,7 @@ class mainController {
     }
     
 
+    // 분류 페이지 - 전체 출력
     async selectList (req, res, next){
         const selectList = await db(`SELECT * FROM category as c, recipe as r WHERE c.category_num = r.category_num`)
 
@@ -27,6 +29,7 @@ class mainController {
     }
 
     
+    // 분류 페이지 - 카테고리 선택해서 출력
     async selectListDetail (req, res, next) {
         const detail = await db(`SELECT * FROM category as c, recipe as r WHERE c.category_num = r.category_num AND r.category_num = "${req.params.category_num}"`)
 
