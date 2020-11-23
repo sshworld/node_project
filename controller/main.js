cd0000227df422a7665e99a0f382664590b918d7
moon <yunii23@naver.com>
2020-11-23 19:34:33 +0900

    뭐수정했지

--- controller/main.js
+++ controller/main.js
@@ -5,12 +5,16 @@ class mainController {
 
         const searchInfo = await db(`SELECT * FROM recipe WHERE recipe_name LIKE "%${req.session.recipe_name}%"`)
 
-        //const scoreInfo = await db(`SELECT * FROM recipe WHERE `)
+        const scoreInfo = await db(`SELECT * FROM recipe ORDER BY recipe_score DESC limit 3`)
 
-        req.body.searchInfo = searchInfo;
+        const Info = {
+            searchInfo : searchInfo,
+            scoreInfo : scoreInfo
+        }
+
+        req.body.Info = Info;
     }
     
 }
   
 module.exports = mainController;
-  
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const mainRoute = require('./main')
 const userRoute = require('./user')
+const orderRoute = require('./order')
 
 const router = express.Router();
 
 router.use("/", mainRoute)
 router.use("/user", userRoute)
+router.use("/order", orderRoute)
 
 
 module.exports = router;
