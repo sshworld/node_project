5f3dac7a7cdcdebd2adb9d3be2bed0aec66709d8
moon <yunii23@naver.com>
2020-12-17 22:38:31 +0900

    메인, 상세

--- controller/main.js
+++ controller/main.js
@@ -1,3 +1,4 @@
+const { NetworkAuthenticationRequire } = require("http-errors");
 const db = require("../middleware/db")
 
 class mainController {
@@ -8,13 +9,16 @@ class mainController {
 
         const scoreInfo = await db(`SELECT * FROM recipe ORDER BY recipe_score DESC limit 3`)
 
+        // 주문수, 리뷰수, 평점
+        //const orderCount = await db (`SELECT COUNT(order_num) as orders, COUNT(reveiw_num) as review FROM orders as o, recipe as r, orderinfo as i ORDER BY recipe_score DESC limit 3 `)
+
         const Info = {
             searchInfo : searchInfo,
             scoreInfo : scoreInfo
         }
 
         req.body.Info = Info;
-
+        console.log(req.body.Info);
         next();
     }
     
@@ -35,6 +39,23 @@ class mainController {
 
         req.body.detail = detail
 
+        next();
+    }
+
+    // 레시피 상세
+    async recipeDetail (req, res, next) {
+        const sql = await db(`SELECT * FROM recipe WHERE recipe_num = "${req.params.recipe_num}"`)
+        const review = await db(`SELECT * FROM review WHERE recipe_num = "${req.params.recipe_num}" ORDER BY review_date DESC`)
+        const reviewCount = await db(`SELECT COUNT(review_num) FROM review WHERE recipe_num = "${req.params.recipe_num}"`)
+
+        const Detail = {
+            DetailInfo : sql[0],
+            Review : review,
+            Count : reviewCount[0]
+        }
+
+        req.body.Detail = Detail;
+        console.log(Detail);
         next();
     }
 }
